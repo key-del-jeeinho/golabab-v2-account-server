001382319837686e5ad5ade20954ad1f44f41b8d
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-01 13:36:47 +0900

    Initial commit

---
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/GolababV2AccountServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.key_del_jeeinho.golabab_v2.account_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolababV2AccountServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolababV2AccountServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/key_del_jeeinho/golabab_v2/account_server/GolababV2AccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.key_del_jeeinho.golabab_v2.account_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolababV2AccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
