e3ed36765de4b2cc5e2b71e308fa8eb30cc92d40
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-09 10:25:29 +0900

    [FIX] AccountDto 필요없는 주석 제거 및 javadoc 수정

--- src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/account/dto/AccountDto.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/account/dto/AccountDto.java
@@ -2,18 +2,21 @@ package io.github.key_del_jeeinho.golabab_v2.account_server.domain.account.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.key_del_jeeinho.golabab_v2.rosetta.account.Role;
-import io.github.key_del_jeeinho.golabab_v2.rosetta.account.UnauthorizedAccountDto;
 
 /**
  * 해당서비스를 이용하는 사용자들의 계정 정보를 담고있는 DTO 입니다.
  * 주로 투표 참여 및 관리시 접근 권한을 확인하기위해 사용됩니다.
  *
  * @author 지인호
- * @see UnauthorizedAccountDto
+ *
+ * @param id 계정의 ID입니다.
+ * @param email 계정의 이메일 입니다.
+ * @param role 계정의 직책입니다.
+ * @param discordId 계정의 디스코드 아이디 입니다.
  */
 public record AccountDto (
         @JsonProperty("id") long id,
-        @JsonProperty("email") String email, //인증이 완료되어 사용자의 소유로 판명된 학교 이메일 입니다.
-        @JsonProperty("role") Role role, //사용자의 직책입니다.
-        @JsonProperty("discordId") long discordId //사용자의 디스코드 아이디 입니다.
+        @JsonProperty("email") String email,
+        @JsonProperty("role") Role role,
+        @JsonProperty("discordId") long discordId
 ){}
