254b0cdd9caa6beed8b8328eaef56130755afe36
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-09 17:01:11 +0900

    [ADD] Controller 단의 getAuthorizeLink Feature 전반 구현

--- src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/authorize/controller/AuthorizeController.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/authorize/controller/AuthorizeController.java
@@ -3,24 +3,23 @@ package io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.con
 import io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.dto.UnauthorizedAccountDto;
 import io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.service.AuthorizeService;
 import io.github.key_del_jeeinho.golabab_v2.rosetta.authorize.request.GetAuthorizeLinkRequest;
+import io.github.key_del_jeeinho.golabab_v2.rosetta.authorize.response.GetAuthorizeLinkResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/authorize-api")
 public class AuthorizeController {
-    private AuthorizeService authorizeService;
+    private final AuthorizeService authorizeService;
 
-    @GetMapping("/authorize-link")
-    public ResponseEntity<String> getAuthorizeLink(@RequestBody GetAuthorizeLinkRequest request) {
+    @PostMapping("/authorize-link")
+    public ResponseEntity<GetAuthorizeLinkResponse> getAuthorizeLink(@RequestBody GetAuthorizeLinkRequest request) {
         UnauthorizedAccountDto account = new UnauthorizedAccountDto(request.email(), request.discordId());
         String link = authorizeService.generateAuthorizeLink(request.callbackUrl(), account, request.limitMinute());
 
-        return ResponseEntity.ok(link);
+        GetAuthorizeLinkResponse response = new GetAuthorizeLinkResponse(link);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/test/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/account/controller/AuthorizeControllerTest.java
@@ -0,0 +1,80 @@
+package io.github.key_del_jeeinho.golabab_v2.account_server.domain.account.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.dto.UnauthorizedAccountDto;
+import io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.service.AuthorizeService;
+import io.github.key_del_jeeinho.golabab_v2.rosetta.authorize.request.GetAuthorizeLinkRequest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@SpringBootTest
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+public class AuthorizeControllerTest {
+    @Autowired
+    private AuthorizeService authorizeService;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("인증링크 요청")
+    public void getAuthorizeLink() throws Exception {
+        GetAuthorizeLinkRequest request = new GetAuthorizeLinkRequest(
+                "www.naver.com",
+                "a@b.c",
+                1000,
+                10
+        );
+        String expectedLink = authorizeService.generateAuthorizeLink(
+                request.callbackUrl(),
+                new UnauthorizedAccountDto(request.email(), request.discordId()),
+                request.limitMinute()
+        );
+        expectedLink = expectedLink.split("\\?")[0];
+
+        String body = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/authorize-api/authorize-link").content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk())
+        .andExpect(jsonPath("$.link").value(Matchers.containsString(expectedLink)))
+
+        .andDo(print())
+        .andDo(document("/docs/{method-name}",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                        fieldWithPath("callbackUrl").description("유저가 인증링크에 접속시, 유저 정보를 전달할 콜백 URL"),
+                        fieldWithPath("email").description("인증 받을유저의 이메일"),
+                        fieldWithPath("discordId").description("인증 받을 유저의 디스코드 아이디"),
+                        fieldWithPath("limitMinute").description("인증링크 유효시간(분)")
+                ),
+                responseFields(
+                        fieldWithPath("link").description("발급한 인증링크")
+                )
+        ));
+    }
+}
