671ee8b91bbf7c4ff099f68524797c879d67d5fc
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-10 00:41:22 +0900

    [ADD] AccountService 의 authorize 메서드를 BDD 방법론으로 작성

--- src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/authorize/service/AuthorizeServiceImpl.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/domain/authorize/service/AuthorizeServiceImpl.java
@@ -3,6 +3,7 @@ package io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.ser
 import io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.dto.AuthorizeResultDto;
 import io.github.key_del_jeeinho.golabab_v2.account_server.domain.authorize.dto.UnauthorizedAccountDto;
 import io.github.key_del_jeeinho.golabab_v2.account_server.global.util.JwtUtil;
+import io.github.key_del_jeeinho.golabab_v2.account_server.global.util.jwt.decoder.JwtDecoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Service;
@@ -19,17 +20,31 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     public String generateAuthorizeLink(String callbackUrl, UnauthorizedAccountDto account, int limitMinute) {
         String address = serverProperties.getAddress().toString();
         Integer port = serverProperties.getPort();
-        String token = JwtUtil.getInstance(
+        String token = JwtUtil.getBuilder(
                 now -> new Date(now.getTime() + Duration.ofMinutes(limitMinute).toMillis()) //현재시각의 limitMinute 분 후
-        )
-                .addClaim("callbackUrl", callbackUrl)
-                .addClaim("account", account)
+        ).addClaim("callbackUrl", callbackUrl)
+                .addClaim("account.discordId", account.discordId())
+                .addClaim("account.email", account.expectedEmail())
         .build();
         return String.format("%s:%d/api/v1/authorize-api/authorize?token=%s", address, port, token);
     }
 
     @Override
-    public AuthorizeResultDto authorize(String token) {
-        return null;
+    public AuthorizeResultDto authorize(String authorizeToken) {
+        //Decoding 로직
+        JwtDecoder decoder = JwtUtil.getDecoder(authorizeToken);
+        String callbackUrl = decoder.get("callbackUrl", String.class);
+        UnauthorizedAccountDto account = new UnauthorizedAccountDto(
+                decoder.get("account.email", String.class),
+                decoder.get("account.discordId", Long.class)
+        );
+        //Encoding 로직
+        String resultToken = JwtUtil.getBuilder(
+                now -> new Date(now.getTime() + Duration.ofMinutes(1).toMillis()) //현재시각의 1분 후
+        ).addClaim("discordId", account.discordId())
+        .addClaim("email", account.expectedEmail())
+        .build();
+        //결과 반환
+        return new AuthorizeResultDto(callbackUrl, resultToken);
     }
 }
