d01dd7ea9e2623d8e175710dfdd08eeb6af300f1
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-10 13:42:14 +0900

    [FIX] 자동 WebMVC Configuration 구성을 위한 EnableWebMvc 어노테이션 제거

--- src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/GolababV2AccountServerApplication.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/GolababV2AccountServerApplication.java
@@ -2,11 +2,8 @@ package io.github.key_del_jeeinho.golabab_v2.account_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class GolababV2AccountServerApplication {
 
     public static void main(String[] args) {
