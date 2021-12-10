87e8193427bc983bf0a49e3888c6b87feab5cb08
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-10 10:58:58 +0900

    [FIX] JwtUtil 로직 일부 변경 (issuer 검사 제외, secret, issuer 를 jwt props 로 초기화)

--- src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/global/util/jwt/decoder/DecoderStrategyImpl.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/global/util/jwt/decoder/DecoderStrategyImpl.java
@@ -1,15 +1,25 @@
 package io.github.key_del_jeeinho.golabab_v2.account_server.global.util.jwt.decoder;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.github.key_del_jeeinho.golabab_v2.account_server.global.property.JwtProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
+@Component //static 변수들을 spring boot property 로 초기화 하기 위해 사용한다.
 public class DecoderStrategyImpl implements DecoderStrategy {
-    @Value("${jwt.secret}")
-    private static String secret = "secret";
-    @Value("${jwt.issuer}")
-    private static String issuer = "issuer";
+    private static String secret;
+
+    @Autowired
+    private JwtProperty jwtProperty;
+
+    @PostConstruct //jwt property 를 통해 secret, issuer 를 초기화한다.
+    private void init() {
+        secret = jwtProperty.getSecret();
+    }
 
     @Override
     public JwtDecoder getInstance(String token) {
-        return new JwtDecoder(token, secret, issuer);
+        return new JwtDecoder(token, secret);
     }
 }
--- src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/global/util/jwt/decoder/JwtDecoder.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/account_server/global/util/jwt/decoder/JwtDecoder.java
@@ -5,26 +5,20 @@ import io.jsonwebtoken.Jwts;
 
 public class JwtDecoder {
     private final String secret;
-    private final String issuer;
     private final Claims claims;
 
-    public JwtDecoder(String token, String secret, String issuer) {
+    public JwtDecoder(String token, String secret) {
         this.secret = secret;
-        this.issuer = issuer;
         claims = parse(token);
     }
 
     private Claims parse(String token) {
         if(token == null) throw new IllegalArgumentException("token is null");
 
-        Claims claims = Jwts.parser()
+        return Jwts.parser()
                 .setSigningKey(secret)
                 .parseClaimsJws(token)
                 .getBody();
-
-        if(!claims.getIssuer().equals(issuer)) throw new IllegalArgumentException("invalid issuer");
-
-        return claims;
     }
 
     public <T> T get(String key, Class<T> clazz) {
